d8482e6a4aba200419cff11f2e55651ee7bd4a79
Dimas Galang Prasetyo <dimasgalangprasetyoo@gmail.com>
2026-04-03 20:16:05 +0700

    Praktikum_CI4

---
+++ app/Views/about.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ <?= $this->include('template/header'); ?>
+<h1><?= $title; ?></h1>
+<hr>
+<p><?= $content; ?></p>
+<?= $this->include('template/footer'); ?>
+</body>
+</html>
